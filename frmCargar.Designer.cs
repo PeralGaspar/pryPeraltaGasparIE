26c7fba78289b4a2408294d58e2932f4497e47d9
root <mgiraudo@ies21.edu.ar>
2023-09-18 13:52:22 -0300

    Agregado cargar Aseguradores

--- frmCargar.Designer.cs
+++ frmCargar.Designer.cs
@@ -31,9 +31,26 @@ namespace pryPeraltaGasparIE
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
             this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.grpDatos = new System.Windows.Forms.GroupBox();
+            this.lblEnt = new System.Windows.Forms.Label();
+            this.lblAper = new System.Windows.Forms.Label();
+            this.lblNExpe = new System.Windows.Forms.Label();
+            this.lblJuz = new System.Windows.Forms.Label();
+            this.lblJuriz = new System.Windows.Forms.Label();
+            this.lblDir = new System.Windows.Forms.Label();
+            this.lblRespon = new System.Windows.Forms.Label();
+            this.txtEnt = new System.Windows.Forms.TextBox();
+            this.txtAper = new System.Windows.Forms.TextBox();
+            this.txtNExpe = new System.Windows.Forms.TextBox();
+            this.txtJuz = new System.Windows.Forms.TextBox();
+            this.txtJuriz = new System.Windows.Forms.TextBox();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.txtRespon = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.grpDatos.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -49,42 +66,189 @@ namespace pryPeraltaGasparIE
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 177);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(521, 171);
             this.dataGridView1.TabIndex = 1;
             // 
-            // button2
+            // btnGrabar
             // 
-            this.button2.Location = new System.Drawing.Point(157, 391);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnGrabar.Location = new System.Drawing.Point(380, 186);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(100, 35);
+            this.btnGrabar.TabIndex = 2;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.button2_Click);
             // 
             // txtNum
             // 
-            this.txtNum.Location = new System.Drawing.Point(255, 393);
+            this.txtNum.Location = new System.Drawing.Point(106, 26);
             this.txtNum.Name = "txtNum";
             this.txtNum.Size = new System.Drawing.Size(100, 20);
             this.txtNum.TabIndex = 3;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(6, 29);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(19, 13);
+            this.lblNum.TabIndex = 4;
+            this.lblNum.Text = "Nº";
+            // 
+            // grpDatos
+            // 
+            this.grpDatos.Controls.Add(this.txtRespon);
+            this.grpDatos.Controls.Add(this.txtDir);
+            this.grpDatos.Controls.Add(this.txtJuriz);
+            this.grpDatos.Controls.Add(this.txtJuz);
+            this.grpDatos.Controls.Add(this.txtNExpe);
+            this.grpDatos.Controls.Add(this.txtAper);
+            this.grpDatos.Controls.Add(this.txtEnt);
+            this.grpDatos.Controls.Add(this.lblRespon);
+            this.grpDatos.Controls.Add(this.lblDir);
+            this.grpDatos.Controls.Add(this.lblJuriz);
+            this.grpDatos.Controls.Add(this.lblJuz);
+            this.grpDatos.Controls.Add(this.lblNExpe);
+            this.grpDatos.Controls.Add(this.lblAper);
+            this.grpDatos.Controls.Add(this.lblEnt);
+            this.grpDatos.Controls.Add(this.lblNum);
+            this.grpDatos.Controls.Add(this.btnGrabar);
+            this.grpDatos.Controls.Add(this.txtNum);
+            this.grpDatos.Location = new System.Drawing.Point(12, 243);
+            this.grpDatos.Name = "grpDatos";
+            this.grpDatos.Size = new System.Drawing.Size(521, 235);
+            this.grpDatos.TabIndex = 5;
+            this.grpDatos.TabStop = false;
+            this.grpDatos.Text = "Datos";
+            // 
+            // lblEnt
+            // 
+            this.lblEnt.AutoSize = true;
+            this.lblEnt.Location = new System.Drawing.Point(6, 65);
+            this.lblEnt.Name = "lblEnt";
+            this.lblEnt.Size = new System.Drawing.Size(43, 13);
+            this.lblEnt.TabIndex = 5;
+            this.lblEnt.Text = "Entidad";
+            // 
+            // lblAper
+            // 
+            this.lblAper.AutoSize = true;
+            this.lblAper.Location = new System.Drawing.Point(6, 101);
+            this.lblAper.Name = "lblAper";
+            this.lblAper.Size = new System.Drawing.Size(47, 13);
+            this.lblAper.TabIndex = 6;
+            this.lblAper.Text = "Apertura";
+            // 
+            // lblNExpe
+            // 
+            this.lblNExpe.AutoSize = true;
+            this.lblNExpe.Location = new System.Drawing.Point(6, 138);
+            this.lblNExpe.Name = "lblNExpe";
+            this.lblNExpe.Size = new System.Drawing.Size(75, 13);
+            this.lblNExpe.TabIndex = 7;
+            this.lblNExpe.Text = "Nº Expediente";
+            // 
+            // lblJuz
+            // 
+            this.lblJuz.AutoSize = true;
+            this.lblJuz.Location = new System.Drawing.Point(232, 29);
+            this.lblJuz.Name = "lblJuz";
+            this.lblJuz.Size = new System.Drawing.Size(47, 13);
+            this.lblJuz.TabIndex = 8;
+            this.lblJuz.Text = "Juzgado";
+            // 
+            // lblJuriz
+            // 
+            this.lblJuriz.AutoSize = true;
+            this.lblJuriz.Location = new System.Drawing.Point(227, 65);
+            this.lblJuriz.Name = "lblJuriz";
+            this.lblJuriz.Size = new System.Drawing.Size(62, 13);
+            this.lblJuriz.TabIndex = 9;
+            this.lblJuriz.Text = "Jurizdicción";
+            // 
+            // lblDir
+            // 
+            this.lblDir.AutoSize = true;
+            this.lblDir.Location = new System.Drawing.Point(227, 97);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(52, 13);
+            this.lblDir.TabIndex = 10;
+            this.lblDir.Text = "Dirección";
+            // 
+            // lblRespon
+            // 
+            this.lblRespon.AutoSize = true;
+            this.lblRespon.Location = new System.Drawing.Point(227, 138);
+            this.lblRespon.Name = "lblRespon";
+            this.lblRespon.Size = new System.Drawing.Size(121, 13);
+            this.lblRespon.TabIndex = 11;
+            this.lblRespon.Text = "Liquidador Responsable";
+            // 
+            // txtEnt
+            // 
+            this.txtEnt.Location = new System.Drawing.Point(106, 62);
+            this.txtEnt.Name = "txtEnt";
+            this.txtEnt.Size = new System.Drawing.Size(100, 20);
+            this.txtEnt.TabIndex = 12;
+            // 
+            // txtAper
+            // 
+            this.txtAper.Location = new System.Drawing.Point(106, 98);
+            this.txtAper.Name = "txtAper";
+            this.txtAper.Size = new System.Drawing.Size(100, 20);
+            this.txtAper.TabIndex = 13;
+            // 
+            // txtNExpe
+            // 
+            this.txtNExpe.Location = new System.Drawing.Point(106, 135);
+            this.txtNExpe.Name = "txtNExpe";
+            this.txtNExpe.Size = new System.Drawing.Size(100, 20);
+            this.txtNExpe.TabIndex = 14;
+            // 
+            // txtJuz
+            // 
+            this.txtJuz.Location = new System.Drawing.Point(380, 26);
+            this.txtJuz.Name = "txtJuz";
+            this.txtJuz.Size = new System.Drawing.Size(100, 20);
+            this.txtJuz.TabIndex = 15;
+            // 
+            // txtJuriz
+            // 
+            this.txtJuriz.Location = new System.Drawing.Point(380, 62);
+            this.txtJuriz.Name = "txtJuriz";
+            this.txtJuriz.Size = new System.Drawing.Size(100, 20);
+            this.txtJuriz.TabIndex = 16;
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(380, 94);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(100, 20);
+            this.txtDir.TabIndex = 17;
+            // 
+            // txtRespon
+            // 
+            this.txtRespon.Location = new System.Drawing.Point(380, 135);
+            this.txtRespon.Name = "txtRespon";
+            this.txtRespon.Size = new System.Drawing.Size(100, 20);
+            this.txtRespon.TabIndex = 18;
             // 
             // frmCargar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 450);
-            this.Controls.Add(this.txtNum);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(545, 493);
+            this.Controls.Add(this.grpDatos);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "frmCargar";
             this.Text = "frmCargar";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.grpDatos.ResumeLayout(false);
+            this.grpDatos.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -92,7 +256,23 @@ namespace pryPeraltaGasparIE
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGrabar;
         private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.GroupBox grpDatos;
+        private System.Windows.Forms.TextBox txtRespon;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.TextBox txtJuriz;
+        private System.Windows.Forms.TextBox txtJuz;
+        private System.Windows.Forms.TextBox txtNExpe;
+        private System.Windows.Forms.TextBox txtAper;
+        private System.Windows.Forms.TextBox txtEnt;
+        private System.Windows.Forms.Label lblRespon;
+        private System.Windows.Forms.Label lblDir;
+        private System.Windows.Forms.Label lblJuriz;
+        private System.Windows.Forms.Label lblJuz;
+        private System.Windows.Forms.Label lblNExpe;
+        private System.Windows.Forms.Label lblAper;
+        private System.Windows.Forms.Label lblEnt;
     }
 }
--- frmCargar.cs
+++ frmCargar.cs
@@ -22,7 +22,7 @@ namespace pryPeraltaGasparIE
         {
             string leerlinea = "";
             string[] separarDatos = new string[0];
-            StreamReader grilla = new StreamReader("C:/Users/Alumnos/source/repos/pryPeraltaGasparIE/Resources/Listado de aseguradores.csv");
+            StreamReader grilla = new StreamReader(@"../../" + "Resources/Listado de aseguradores.csv");
             leerlinea = grilla.ReadLine();
             separarDatos = leerlinea.Split(';');
             foreach (String var in separarDatos)
@@ -40,13 +40,30 @@ namespace pryPeraltaGasparIE
 
         private void button2_Click(object sender, EventArgs e)
         {
-            StreamWriter objSw = new StreamWriter("ejemploProveedor", true);
-            objSw.WriteLine(txtNum.Text);
+            String nuevo = "";
+            StreamWriter objSw = new StreamWriter(@"../../" + "Resources/Listado de aseguradores.csv", true);
+            nuevo = txtNum.Text + ";" + txtEnt.Text + ";" + txtAper.Text + ";" + txtNExpe + ";"
+                + txtJuz.Text + ";" + txtJuriz.Text + ";" + txtDir.Text + ";" + txtRespon.Text;
+            objSw.WriteLine(nuevo);
             
             objSw.Close();
             MessageBox.Show("Registrado");
             txtNum.Text = "";
+            txtAper.Text = "";
+            txtDir.Text = "";
+            txtEnt.Text = "";
+            txtJuriz.Text = "";
+            txtJuz.Text = "";
+            txtNExpe.Text = "";
+            txtRespon.Text = "";
             txtNum.Focus();
+            txtAper.Focus();
+            txtDir.Focus();
+            txtEnt.Focus();
+            txtJuriz.Focus();
+            txtJuz.Focus();
+            txtNExpe.Focus();
+            txtRespon.Focus();
         }
     }
 }
